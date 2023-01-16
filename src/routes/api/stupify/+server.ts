f6a3fec285783a1066204265aaf4a54a50c977f5
Constantinos Karaolis <kostaskaraolis@gmail.com>
2023-01-16 11:01:19 +0200

    implementation of stupifier

---
+++ src/routes/api/stupify/+server.ts
@@ -0,0 +1,20 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+function stupify(text: string): string {
+	let stupified = '';
+	for (let i = 0; i < text.length; i++) {
+		if (i % 2 === 0) {
+			stupified += text[i].toUpperCase();
+		} else {
+			stupified += text[i].toLowerCase();
+		}
+	}
+	return stupified;
+}
+
+export const POST = (async ({ request }) => {
+	const body = await request.json();
+	const response = json(stupify(body.textToBeStupified), { status: 200 });
+	return response;
+}) satisfies RequestHandler;
